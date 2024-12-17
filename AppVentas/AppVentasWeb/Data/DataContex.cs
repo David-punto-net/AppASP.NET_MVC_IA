10111ac065b3388664c34c26b7ed037d3f0579a6
David <davidtorresn@gmail.com>
2024-12-17 15:10:21 -0300

    se agrega dbcontext

---
+++ AppVentas/AppVentasWeb/Data/DataContex.cs
@@ -0,0 +1,21 @@
+﻿using AppVentasWeb.Data.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppVentasWeb.Data
+{
+    public class DataContex : DbContext
+    {
+        public DataContex(DbContextOptions<DataContex> options ): base(options) 
+        {
+            
+        }
+
+        public DbSet<Pais> Paises { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Pais>().HasIndex(p => p.Nombre).IsUnique();
+        }
+    }
+}
---
+++ AppVentas/AppVentasWeb/Data/Entidades/Pais.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppVentasWeb.Data.Entidades
+{
+    public class Pais
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Debe ingresar el {0}")]
+        [StringLength(50, ErrorMessage ="El campo {0} debe tener máximo {1} caractéres")]
+        [Display(Name ="País")]
+        public string Nombre { get; set; }
+    }
+}
