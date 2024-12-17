9daeaa416a1030b32701d3475aeb2986ae5263fb
David <davidtorresn@gmail.com>
2024-12-17 10:10:35 -0300

    Creación del proyecto AppVentasWeb

---
+++ AppVentas/AppVentasWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppVentasWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
