936e01ca297f8069cca6d054e0d91894638e09cb
David <davidtorresn@gmail.com>
2024-12-30 20:51:29 -0300

    nuevo ajuste al home

--- AppVentas/AppVentasWeb/Controllers/HomeController.cs
+++ AppVentas/AppVentasWeb/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
+using AppVentasWeb.Data;
+using AppVentasWeb.Data.Entidades;
 using AppVentasWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace AppVentasWeb.Controllers
@@ -7,15 +10,47 @@ namespace AppVentasWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContex _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContex context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Producto>? productos = await _context.Productos
+            .Include(p => p.ProductImages)
+            .Include(p => p.ProductCategories)
+            .OrderBy(p => p.Description)
+            .ToListAsync();
+
+            List<ProductsHomeViewModel> productsHome = new() { new ProductsHomeViewModel() };
+            int i = 1;
+            foreach (Producto? producto in productos)
+            {
+                if (i == 1)
+                {
+                    productsHome.LastOrDefault().Product1 = producto;
+                }
+                if (i == 2)
+                {
+                    productsHome.LastOrDefault().Product2 = producto;
+                }
+                if (i == 3)
+                {
+                    productsHome.LastOrDefault().Product3 = producto;
+                }
+                if (i == 4)
+                {
+                    productsHome.LastOrDefault().Product4 = producto;
+                    productsHome.Add(new ProductsHomeViewModel());
+                    i = 0;
+                }
+                i++;
+            }
+            return View(productsHome);
         }
 
         public IActionResult Privacy()
@@ -34,6 +69,5 @@ namespace AppVentasWeb.Controllers
         {
             return View();
         }
-
     }
 }
---
+++ AppVentas/AppVentasWeb/Models/ProductsHomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AppVentasWeb.Data.Entidades;
+
+namespace AppVentasWeb.Models
+{
+    public class ProductsHomeViewModel
+    {
+        public Producto Product1 { get; set; }
+        public Producto Product2 { get; set; }
+        public Producto Product3 { get; set; }
+        public Producto Product4 { get; set; }
+    }
+}
